d3f5c8d46a74f99ac8f6ce40134f4f829b077e68
daydayday <kdu2036@gmail.com>
2026-09-12 20:19:36 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // 오프라인 지원. 파일을 고친 뒤에는 VERSION 을 올려야 갱신됩니다.
-var VERSION = "v6";
+var VERSION = "v1";
 var ASSETS = [
   "./", "./index.html", "./css/style.css", "./js/app.js",
-  "./data/a.js", "./data/b.js", "./data/c.js", "./manifest.webmanifest"
+  "./data/dumun.js", "./data/danmun.js", "./manifest.webmanifest"
 ];
 
 self.addEventListener("install", function (e) {
